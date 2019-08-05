872744b3bf716c419050eb3f343fda45c5934e4b
Syed Shahiq <shahiqurrehman@gmail.com>
2019-08-05 11:28:26 +0500

    Axios added

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 import './FullPost.css';
 
 class FullPost extends Component {
@@ -9,7 +9,7 @@ class FullPost extends Component {
   componentDidUpdate() {
     if (this.props.id) {
       if ((!this.state.loadedPost) || ((this.state.loadedPost) && (this.state.loadedPost.id !== this.props.id))) {
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        axios.get('/posts/' + this.props.id)
           .then(response => {
             this.setState({ loadedPost: response.data });
           })
@@ -21,7 +21,7 @@ class FullPost extends Component {
     }
   }
   deletePostHandler = () => {
-    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+    axios.delete('/posts/' + this.props.id)
       .then(response => {
         console.log(response);
       })
